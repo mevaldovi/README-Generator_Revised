0174f5a41eebc6df0ae03559ad38b5152eb42f23
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-08-21 17:05:25 -0500

    new changes

--- index.js
+++ index.js
@@ -1,32 +1,82 @@
 // TODO: Include packages needed for this application = DONE
+
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const express = require("express");
-const app = express();
-const PORT = 3000;
+const generateMarkdown = require('./utils/generateMarkdown');
 
-app.set("view file", "index.html"); //tells app to view file
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: "What is your project's title?",
+        name: "title",
+    }, 
+    {
+        type: 'input',
+        message: "What is your project's description?",
+        name: "description",
+    },
+    {
+        type: 'input',
+        message: "How do you install your project?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "What can you do with your project?",
+        name: "usage",
+    },
+    {
+        type: "list",
+        message: "What license are you using?",
+        name: "license",
+        choices: [
+            "None",
+            "Apache License 2.0",
+            "GNU General Public License V3.0",
+            "MIT License",
+            "BSD 3-Clause"
+        ]
+    },
+    {
+        type: "input",
+        message: "What are the contribution guidlines?",
+        name: "help",
+    }, 
+    {
+        type: "input",
+        message: "Test Instructions?",
+        name: "test",
+    },
+    {
+        type: "input",
+        message: "Please enter your github username",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "Please enter your email address",
+        name: "email"
+    }
+];
 
-app.get("/", (req, res) => {
-    res.render("index"); // index refers to index.html
-});
-
-app.listen(3000, () => {
-        console.log("server started on port 3000")
-    }) //telling the port to listen for the server doing its thing
-
-
-//create a promise for each of the questions to load;
-.then((response) => { //
-    console.log(response);
-    writeToFile('indexHTML', generateMarkdown(response)); //write out the files backbone and place all the content from generateMarkdown inside of it !
-    console.log("generated markdown!");
-})
-
-
+// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
-    fs.writeFile(fileName, data, (err) =>
-        err ? console.log(err) : console.log('Successfully created indec.html!')
+    //fill template in with recorded data
+    fs.writeFile(`${fileName}.md`, generateMarkdown(data), (err) =>
+    err ? console.error(err) : console.log('Success!')
     );
 }
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((response) => 
+    //write the recorded data to a README.md file
+        writeToFile("README", response),
+    );
+}
+
+// Function call to initialize app
+init();
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,81 +1,74 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license !== null) {
-    console.log("license exists");
-  } else {
-    console.log("  ");
+  switch(license)
+  {
+    case "None":
+      return "";
+    case "Apache License 2.0":
+      return "https://img.shields.io/badge/License-Apache%202.0-blue.svg";
+    case "GNU General Public License V3.0":
+      return "https://img.shields.io/badge/License-GPL%20v3-blue.svg";
+    case "MIT License":
+      return "https://img.shields.io/badge/License-MIT-yellow.svg";
+    case "BSD 3-Clause":
+      return "https://img.shields.io/badge/License-BSD%203--Clause-blue.svg";
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license.url !== null) {
-    console.log("url exists");
-  } else {
-    console.log("  ");
+  switch(license)
+  {
+    case "None":
+      return "";
+    case "Apache License 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "GNU General Public License V3.0":
+      return "http://www.gnu.org/licenses/gpl-3.0";
+    case "MIT License":
+      return "https://opensource.org/licenses/MIT";
+    case "BSD 3-Clause":
+      return "https://opensource.org/licenses/BSD-3-Clause";
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license.section !== null) {
-    console.log("license section exists");
-  } else {
-    console.log("  ");
-  }
+  if (license == "None")
+  {return "";}
+  else
+  {return `[![License](${renderLicenseBadge(license)})](${renderLicenseLink(license)})`;}
 }
 
-// TODO: Create a function to generate markdown for README
+
+
+//create function  that will generate README template that will hold the data user enters from inquirer
 function generateMarkdown(data) {
   return `# ${data.title}
-    # ${response.title}
-      
-      # Table of Content
-      -[description](#description)
-
-      -[installation](#installation)
-
-      -[usage](#usage)
-
-      -[licenses](#licenses)
-
-      -[contribution](#contribution)
-
-      -[test](#test)
-
-      -[username](#username)
-
-      -[profile](#profile)
-      
-      
-          ${response.description}
-      ##description:
-
-        ${response.technology}
-        ##technology:
-      
-          ${response.installation}
-      ##installation:
-      
-          ${response.usage}
-      ##usage:
-      
-          ${response.licenses}
-      ##licenses:
-      
-          ${response.agknowledgements}
-      ##agknowledgements:
-      
-          ${response.test}
-      ##test:
-
-      ${response.contact}
-      ##contact:
-
-        
+  ${renderLicenseSection(data.license)}
+## Description
+${data.description}
+## Table of Contents
+[Installation](#Installation)  
+[Usage](#Usage)  
+[License](#License)  
+[Contributing](#Contribution-Guidelines)  
+[Tests](#How-To-Test)  
+[COntact](#Contact)  
+## Installation
+\`\`\`${data.install}\`\`\`
+## Usage
+${data.usage}
+## Contribution Guidelines
+${data.help}
+## How to Test
+${data.test}
+## Contact
+[github](https://github.com/${data.github})
+${data.email}  
 `;
 }
 
